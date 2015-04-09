c36d4881b47ad125148ba5c96246c543d027c7fc
Raphael <raphael.d@yumasoft.com>
2015-04-09 22:47:18 +0300

    locale, remove comments

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,25 +1,4 @@
 var buttonClicked = function() {
-  // var version = localStorage["jquery_version"] || "2.1.1";
-    //var script = [];
-    //var scriptSource = 'http://localhost:2341/app/mainEmbedded.js';
-    //script.push('if (typeOf(contextCommentPanelOpen) == 'function'){')
-    //script.push('var head = document.getElementsByTagName("head")[0];');
-    //script.push('var script = document.createElement("script");');
-    //script.push('script.type = "text/javascript";');
-    //script.push('script.src = "' + scriptSource + '";');
-    //script.push('script.onload = function(){window.contextCommentPanelOpen();};');
-    //script.push('script.onload = function(){console.log(window);};');
-    
-    //script.push('head.appendChild(script);');
-    //chrome.tabs.executeScript(null, { code: script.join('') });
     chrome.tabs.executeScript(null, {file: "content.js"});
-    //chrome.tabs.sendMessage(integer tabId, any message, object options, function responseCallback)
-    //contextCommentPanelOpen
-  // chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
-  // chrome.browserAction.setBadgeText({text:version});
-  // chrome.alarms.create('showBadgeText', {when: Date.now() + 3000});
-  // chrome.alarms.onAlarm.addListener(function() {
-    // chrome.browserAction.setBadgeText({text:""});
-  // });
 };
 chrome.browserAction.onClicked.addListener(buttonClicked);
